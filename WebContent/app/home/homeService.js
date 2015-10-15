ba6509c1ea4a72305cab5b9470c047dcf91869a1
James Hill <jhill@scottlogic.com>
2015-10-15 14:10:56 +0100

    Written basic code to gather information for specified company and duration

--- WebContent/app/home/homeService.js
+++ WebContent/app/home/homeService.js
@@ -10,13 +10,13 @@
 	}
 	
 	HomeService.prototype = {
-			getDataForCompany : function(companyCode, callback) {
-				var startDate = "2009-09-11";
-				var endDate = "2010-03-10";
+			getDataForCompany : function(companyCode, callback, startDate, endDate) {
+				if (!startDate) { startDate = "2009-09-11"; }
+				if (!endDate) { endDate = "2011-03-10"; }
 				var completeQuery = "https://query.yahooapis.com/v1/public/yql?q=select Date, Open, Close from yahoo.finance.historicaldata where symbol = '" + companyCode +
 									"' and startDate = '" + startDate + "' and endDate = '" + endDate + "'&format=json&env=http://datatables.org/alltables.env&callback=";
-				console.log(completeQuery);
 				this.$http.get(completeQuery).then(function(jsonResponse) {
+					console.log(jsonResponse);
 					if (callback) { callback(jsonResponse); }
 				}, function(errorResponse) {
 					if (errorResponse.status !== 200) {
